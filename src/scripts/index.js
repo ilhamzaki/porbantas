be184bdfde3083da76855ee6449730d09e44ef39
ilham zaki <ilhamzaki32@gmail.com>
2021-12-01 08:45:51 +0700

    Membuat Application Shell dan Mengkonfigurasi Drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,10 @@
 import 'regenerator-runtime';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/style.css';
+import App from './views/app';
 
-console.log('Hello Coders!');
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
