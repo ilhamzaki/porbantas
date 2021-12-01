45e4352a0748b004f68863f0dd3b696aa17fde86
ilham zaki <ilhamzaki32@gmail.com>
2021-12-01 09:52:25 +0700

    Melakukan Request API dan Menampilkan Konten

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  KEY: '4a1c0fd7b4b749818b35ff058e50912a',
+  BASE_URL: 'https://newsapi.org/v2/top-headlines?country=id',
+};
+
+export default CONFIG;
---
+++ src/scripts/views/pages/bookmark.js
@@ -0,0 +1,13 @@
+const Bookmark = {
+  async render() {
+    return `
+                <h2>Bookmark Page</h2>
+            `;
+  },
+
+  async afterRender() {
+
+  },
+};
+
+export default Bookmark;
