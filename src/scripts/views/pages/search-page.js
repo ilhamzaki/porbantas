2e97a13095926b4b0074928e18b7980a672d5e7c
ilham zaki <ilhamzaki32@gmail.com>
2021-12-03 10:00:51 +0700

    Membuat pencarian berdasarkan title

---
+++ src/scripts/views/pages/search-page.js
@@ -0,0 +1,31 @@
+/* eslint-disable max-len */
+import NewsSource from '../../data/news-source';
+import createNewsItemTemplate from '../templates/template-creator';
+
+const SearchPage = {
+  async render() {
+    return `
+    <div class="content">
+        <h5 id="heading-search" class="content__heading"></h5>
+        <div id="news" class="news">
+
+        </div>
+    </div>
+        `;
+  },
+
+  async afterRender() {
+    const news = await NewsSource.home();
+    const searchElement = document.querySelector('#search-bar');
+    const newsContainer = document.querySelector('#news');
+    const searchData = news.filter((item) => item.title.toLowerCase().includes(searchElement.value.toLowerCase()));
+    console.log(searchData);
+    console.log(searchElement.value);
+    document.querySelector('#heading-search').innerHTML = `Hasil pencarian "${searchElement.value}"`;
+    searchData.forEach((item) => {
+      newsContainer.innerHTML += createNewsItemTemplate(item);
+    });
+  },
+};
+
+export default SearchPage;
